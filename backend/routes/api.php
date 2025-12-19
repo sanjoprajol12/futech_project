b3cfcaa575759416038ee81f39889653870f01fe
Prajwol <sanjoprajol12@gmail.com>
2025-12-19 12:11:39 +0545

    Remove backend from git

--- backend/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-// routes/api.php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/example', function () {
-    return response()->json(['message' => 'Hello from the backend!']);
-});
